b28f034b5598fe17669cbc0d66ad6760e01d68cf
Thiago Miranda <mirandathiago@gmail.com>
2016-02-12 15:33:32 -0300

    Adicionando o Botão sair ao Perfil do Responsável

--- resources/views/responsavel/perfil.blade.php
+++ resources/views/responsavel/perfil.blade.php
@@ -17,12 +17,16 @@
     }
 
     ?>
-
     <div class="row">
+        <div class="col-md-12 col-sm-12">
+            <a class="btn btn-primary pull-right" href="{!! route('responsavel.sair') !!}">
+                <i class="fa fa-sign-out"></i> Sair
+            </a>
+        </div>
 
-
-
-
+    </div>
+<br>
+    <div class="row">
 
         <div class="col-md-12 col-sm-12">
 
