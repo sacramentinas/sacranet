bdc7bcfea1cd2755a561c6b7e9043c689cdc6d67
Thiago Miranda <mirandathiago@gmail.com>
2016-02-25 09:18:57 -0300

    Correção do Cross Site

--- app/Http/Middleware/AuthResponsavel.php
+++ app/Http/Middleware/AuthResponsavel.php
@@ -31,6 +31,10 @@ class AuthResponsavel
                 return redirect()->guest('auth/login');
             }
         }*/
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');
 
         if(Auth::aluno()->guest())
         {
